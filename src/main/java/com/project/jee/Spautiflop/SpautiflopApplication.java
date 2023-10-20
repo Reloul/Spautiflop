c3a80ea83fb70dd5b47d6b3c59b988e3849b37db
durandnico <nicod4956@gmail.com>
2023-10-20 10:26:54 +0200

    init

---
+++ src/main/java/com/project/jee/Spautiflop/SpautiflopApplication.java
@@ -0,0 +1,13 @@
+package com.project.jee.Spautiflop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpautiflopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpautiflopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/jee/Spautiflop/SpautiflopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jee.Spautiflop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpautiflopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
