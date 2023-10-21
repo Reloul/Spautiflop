05e5ebee6fa82e591f656052cbfc0233b729eece
durandnico <nicod4956@gmail.com>
2023-10-21 14:51:37 +0200

    some checks on file + increase max size file

--- src/main/java/com/project/jee/spautiflop/service/UserService.java
+++ src/main/java/com/project/jee/spautiflop/service/UserService.java
@@ -26,19 +26,25 @@ public class UserService {
     if( this.userRepository.findByPseudoIgnoreCase(userRegistrationBody.getPseudo()).isPresent() )
       throw new UserAlreadyExistsException("User already exists");
 
+    if(userRegistrationBody.getPhoto().isEmpty() || userRegistrationBody.getPhoto().getContentType() == null || !userRegistrationBody.getPhoto().getContentType().startsWith("image/") || !ImageIO.read(userRegistrationBody.getPhoto().getInputStream()).getClass().getSimpleName().equals("BufferedImage"))
+      throw new IOException("File is not an image");
+
     LocalUser user = new LocalUser();
     user.setPseudo(userRegistrationBody.getPseudo());
 
     //TODO : encrypt password
     user.setPassword(userRegistrationBody.getPassword());
 
-    //TODO : save photo in folder and save path in database
+    String filePath = this.pathDirectory + String.valueOf(System.currentTimeMillis()) + userRegistrationBody.getPhoto().getOriginalFilename();
 
-    user.setPhoto(userRegistrationBody.getPhoto().getOriginalFilename());
+    if(filePath.length()>255)
+      throw new IOException("File name too long");
+
+    user.setPhoto(filePath);
 
     System.out.println(pathDirectory);
     try{
-      userRegistrationBody.getPhoto().transferTo(new File(this.pathDirectory+userRegistrationBody.getPhoto().getOriginalFilename()));
+      userRegistrationBody.getPhoto().transferTo(new File(filePath));
     } catch (IOException e) {
       throw new IOException("Error while saving photo  : " + e.getMessage());
     }
