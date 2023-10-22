f55c079384f0989da5f3eded472c6f59768e4800
Valentin Serres <serresvale@gamil.com>
2023-10-22 18:32:31 +0200

    feat(BrancheVal): Front de la page d'accueil et carte musique pas finie

---
+++ src/main/resources/templates/src/view/PageAccueil.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page">
+    <TitreAccueil />
+    <AccueilCentral />
+  </div>
+</template>
+
+<script>
+import TitreAccueil from '../components/TitreAccueil.vue'
+import AccueilCentral from './AccueilCentral.vue'
+
+export default {
+  name: 'PageAccueil',
+  components: {
+    TitreAccueil,
+    AccueilCentral,
+  }
+
+}
+</script>
+
+<style scoped>
+    
+</style>
