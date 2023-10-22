ff28d183452238b12b55479ca63713a01457519c
Valentin Serres <serresvale@gamil.com>
2023-10-22 18:32:31 +0200

    feat(BrancheVal): Front de la page d'accueil et carte musique pas finie

---
+++ src/main/resources/templates/src/components/TitreAccueil.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="header">
+        <h1 id="TitreSite">Spautiflop</h1>
+        <h3><a href="#">Sign Up</a> / <a href="#">Login</a></h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TitreAccueil'
+}
+</script>
+
+<style scoped>
+    #TitreSite{
+        color: white;
+        text-align: left;
+        margin-left: 2vw;
+        margin-top: 4vh;
+    }
+    #header{
+        display: flex;
+    }
+    #header h1{
+        width: 50vw;
+    }
+    #header h3{
+        color: #A7BED3;
+        width: 50vw;
+        text-align: right;
+        float: right;
+        margin-right: 2vw;
+        margin-top: 5vh;
+    }
+    a{
+        color: #A7BED3;
+    }
+</style>
---
+++ src/main/resources/templates/src/view/AccueilCentral.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="Central">
+    <h1>Bienvenu sur Spautiflop, le (presque) premier site de streaming de musique !</h1>
+    <hr>
+    <h2>Découvrez un large choix de musique et faites vos propres Playlist</h2>
+    <CarteMusique :img="img" :music="music" :artist="artist" :nbLike="nbLike" :isLike="isLike" :imgL="imgL"/>
+  </div>
+</template>
+
+<script>
+import CarteMusique from '../components/CarteMusique.vue'
+export default {
+    name: 'AccueilCentral',
+    components: {
+        CarteMusique,
+    },
+    data() {
+        return{
+            img: require('../imgFolder/V4.jpg'),
+            music: 'Moonless',
+            artist: 'Fl3r',
+            nbLike: '8900',
+            isLike: 'true',
+            imgL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/800px-Heart_coraz%C3%B3n.svg.png',
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #Central{
+        background-color: #3b4762;
+        width: 70vw;
+        height: 70vh;
+        border-radius: 10px;
+        text-align: center;
+        margin:auto;
+    }
+    #Central h1{
+        top: 4vh;
+        font-size: 50px;
+        color: #A7BED3;
+        position: relative;
+    }
+    #Central h2{
+        bottom: 4vh;
+    }
+    hr{
+        height: 3px;
+        background-color: #A7BED3;
+        border: none;
+        width: 60vw;
+        margin-top: 8vh;
+        margin-bottom: 5vh;
+    }
+</style>
