436c39d0c22ef4ccdb467811a6b645e88110bbdc
durandnico <nicod4956@gmail.com>
2023-10-24 16:48:40 +0200

    VUETIFYYYY

--- src/main/resources/templates/src/App.vue
+++ src/main/resources/templates/src/App.vue
@@ -1,29 +1,32 @@
 <template>
-  <PageAccueil />
+  <v-app>
+    <v-main class="main">
+      <PageAccueil />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import PageAccueil from './view/PageAccueil.vue'
+import PageAccueil from "./view/PageAccueil.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     PageAccueil,
-  }
-}
+  },
+};
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    top: -2vh;
-    color: #a7bed3;
-  }
-  body{
-    background-color: #273043;
-  }
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  top: -2vh;
+  color: #a7bed3;
+}
+body, .main {
+  background-color: #273043;
+}
 </style>
