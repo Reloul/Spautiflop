31e2b09c50c826ccf43a89b12e39d1945120937e
durandnico <nicod4956@gmail.com>
2023-10-26 23:32:03 +0200

    some usefull code and const

--- src/main/resources/templates/src/util/global.js
+++ src/main/resources/templates/src/util/global.js
@@ -1,1 +1,10 @@
-export const API_URL = "http://localhost:8080";
+export const URL = "http://localhost:8080";
+
+export const CONFLICT = 409;
+export const UNAUTHORIZED = 401;
+export const FORBIDDEN = 403;
+export const NOT_FOUND = 404;
+export const OK = 200;
+export const CREATED = 201;
+export const BAD_REQUEST = 400;
+export const INTERNAL_SERVER_ERROR = 500;
