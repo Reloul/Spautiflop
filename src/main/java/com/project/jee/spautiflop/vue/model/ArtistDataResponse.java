0a27f4d74fcdd2a0dbef76a650f8eabe79f7afff
durandnico <nicod4956@gmail.com>
2023-11-02 13:14:53 +0100

    return top 5 best song

--- src/main/java/com/project/jee/spautiflop/vue/model/ArtistDataResponse.java
+++ src/main/java/com/project/jee/spautiflop/vue/model/ArtistDataResponse.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.project.jee.spautiflop.model.Album;
 import com.project.jee.spautiflop.model.Artist;
 import com.project.jee.spautiflop.model.Song;
+import com.project.jee.spautiflop.model.repo.LikesRepository;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
@@ -43,7 +45,14 @@ public class ArtistDataResponse {
         this.id = artist.getId();
         this.name = artist.getName();
         this.bestSongs = new ArrayList<Song>();
-        this.bestSongs.addAll(artist.getSongs());
+
+        /* top 5 songs */
+        List<Song> top5 = new ArrayList<Song>(artist.getSongs());
+        top5.sort((s1, s2) -> s2.getNbLikes().compareTo(s1.getNbLikes()));
+        for (int i = 0; i < 5 && i < top5.size(); i++) {
+            this.bestSongs.add(top5.get(i));
+        }
+
         this.albums = new ArrayList<Album>();
         this.albums.addAll(artist.getAlbums());
         this.photo = artist.getPhoto();
